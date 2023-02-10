a02ad4a0e3a7ae44d1c4f7f7dd4a5a5268f2be1c
is0705 <108756697+is0705@users.noreply.github.com>
2023-02-10 13:33:38 -0500

    Update main.c

--- main.c
+++ main.c
@@ -18,9 +18,9 @@ const char tree[] = {'-', 'E', 'T', 'I', 'A', 'N', 'M', 'S', 'U', 'R', 'W',
                     '#', '7', '#', '#', '#', '8', '#', '9', '0',
                     '!'};
 
-#define CLK_FREQ 62500
-int rise = 0;
-int fall = -1;
+#define CLK_FREQ 62500UL
+unsigned int rise = 0;
+unsigned int fall = -1;
 int ovfCount = 0;
 float lowTime = 0.0;
 float highTime = 0.0;
@@ -46,6 +46,8 @@ void Initialize() {
     cli();
 
     DDRB &= ~(1<<DDB0); // (ICP1) input pin
+    DDRB |= (1<<DDB2); // dot LED
+    DDRB |= (1<<DDB3); // dash LED
 
     // TOTAL PRESCALING: divide by 256 --> frequency = 62.5 kHz
     // prescale timer clock by factor of 256
@@ -73,42 +75,34 @@ ISR(TIMER1_OVF_vect) {
 }
 
 ISR(TIMER1_CAPT_vect) {
-    cli();
     if (PINB & (1<<PINB0)) { // caught rising edge
         rise = ICR1; // record timer value of rising edge
-        TIFR1 |= (1<<ICF1); // clear interrupt flag
-        // compare fall to rise time
         lowTime = (float)(rise - fall) * 1000 / CLK_FREQ + ovfCount * 1000; // how long button not pressed (ms)
-        if (lowTime >= 800 && fall >= 0) {
-            // space = not pressed for at least 400 x 2 ms
+        if (lowTime >= 1000 && fall >= 0) {
             sprintf(String, "%c\n", tree[current]);
             UART_putstring(String);
             current = 0;
         }
-        ovfCount = 0; // reset overflow count
-        _delay_ms(50);
-        sei();
+        TIFR1 |= (1<<ICF1); // clear interrupt flag
         TCCR1B &= ~(1<<ICES1); // detect falling edge
     } else { // caught falling edge
         fall = ICR1; // record timer value of falling edge
-        TIFR1 |= (1<<ICF1); // clear interrupt flag
-        // compare rise to fall time
         highTime = (float)(fall - rise) * 1000 / CLK_FREQ + ovfCount * 1000; // how long button pressed (ms)
-        if (highTime >= 400 && highTime <= 800) {
-            // dash = pressed for 200x2 ms to 400x2 ms
+        if (highTime >= 500) {
             current = addDash(current);
-        } else if (highTime >= 30 && highTime <= 400) {
-            // dot = pressed for 30 ms to 200 x 2 ms
+            PORTB |= (1<<PORTB3);
+            _delay_ms(50);
+            PORTB &= ~(1<<PORTB3);
+        } else if (highTime < 500) {
             current = addDot(current);
+            PORTB |= (1<<PORTB2);
+            _delay_ms(50);
+            PORTB &= ~(1<<PORTB2);
         }
-        else {
-            current = 0;
-        }
-        ovfCount = 0; // reset overflow count
-        _delay_ms(50);
-        sei();
+        TIFR1 |= (1<<ICF1); // clear interrupt flag
         TCCR1B |= (1<<ICES1); // detect rising edge
     }
+    ovfCount = 0;
 }
 
 int main(void)
